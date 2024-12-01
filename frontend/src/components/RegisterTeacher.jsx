25725a3c8a5685fef81326bc9ba002092bf36a44
Fahad Saleem <mfahadsal216@gmail.com>
2024-12-01 18:48:58 +0500

    message

--- frontend/src/components/RegisterTeacher.jsx
+++ frontend/src/components/RegisterTeacher.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 //import axiosInstance from './axios';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import './registerteacher.css'
 const RegisterTeacher = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
@@ -43,48 +43,62 @@ const RegisterTeacher = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-             <div>
-                <h4>Select Courses:</h4>
-                <label>
-                    <input
-                        type="checkbox"
-                        value="SAT"
-                        onChange={(e) =>
-                            setCourses((prev) => (prev.includes(e.target.value) ? prev : [...prev, e.target.value]))
-                        }
-                    />
-                    SAT
-                </label>
+        <div className='register-teacher-page'>
+            <div className='register-teacher-form-area'>
+                <form onSubmit={handleSubmit} className='register-teacher-form'>
+                    <p>Username</p>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+                    <p>Email</p>
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+                    <p>Password</p>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                    <div className='checkbox-area'>
+                        <h4>Select Courses:</h4>
+                        <div className='single-checkbox'>
+                            <input
+                                type="checkbox"
+                                value="SAT"
+                                onChange={(e) =>
+                                    setCourses((prev) => (prev.includes(e.target.value) ? prev : [...prev, e.target.value]))
+                                }
+                            />
+                            <p>SAT</p>
+                        </div>
+                        <div className='single-checkbox'>
+                            <input
+                                type="checkbox"
+                                value="NAT"
+                                onChange={(e) =>
+                                    setCourses((prev) => (prev.includes(e.target.value) ? prev : [...prev, e.target.value]))
+                                }
+                            />
+                            <p>NAT</p>
+                        </div>
+                        <div className='single-checkbox'>
+                            <input
+                                type="checkbox"
+                                value="NET"
+                                onChange={(e) =>
+                                    setCourses((prev) => (prev.includes(e.target.value) ? prev : [...prev, e.target.value]))
+                                }
+                            />
+                            <p>NET</p>
+                        </div>
+                    </div>
+                    <button type="submit">Register</button>
+                    {error && <p>{error}</p>}
+                </form>
+            </div>
+            <div className='register-text-area'>
+                <p>
+                Your journey
                 <br />
-                <label>
-                    <input
-                        type="checkbox"
-                        value="NAT"
-                        onChange={(e) =>
-                            setCourses((prev) => (prev.includes(e.target.value) ? prev : [...prev, e.target.value]))
-                        }
-                    />
-                    NAT
-                </label>
+                Towards teaching
                 <br />
-                <label>
-                    <input
-                        type="checkbox"
-                        value="NET"
-                        onChange={(e) =>
-                            setCourses((prev) => (prev.includes(e.target.value) ? prev : [...prev, e.target.value]))
-                        }
-                    />
-                    NET
-                </label>
-            </div> 
-            
-            <button type="submit">Register</button>
-        </form>
+                Starts here!
+                </p>  
+            </div>
+        </div>
     );
 }
 
