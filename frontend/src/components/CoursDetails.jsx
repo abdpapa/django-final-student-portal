b3f68721b6f9aa99f4ee50d4690a4fac565f7110
Dyno32 <muhammad.usmann25@gmail.com>
2024-12-01 23:32:41 +0500

    finalised course details

--- frontend/src/components/CoursDetails.jsx
+++ frontend/src/components/CoursDetails.jsx
@@ -1,72 +1,58 @@
-
 //THE STUDENT COURSE PAGE WHEN HE CLICKS ON ONE OF THE BOXES
 
 
 import "./coursedetails.css";
 import React, { useEffect, useState } from "react";
 import Topic from "./topic"; 
-import { Link, useParams } from "react-router-dom";
-import axios from 'axios'
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Navbar from "../Navbar";
+
 const CoursDetails = () => {
+  const { cname } = useParams();
+  const [subjects, setSubjects] = useState([]);
+  const [error, setError] = useState("");
 
-
-  const {cname}=useParams()
-  
-  const[subjects,setSubjects]=useState([])
- const[error,setError]=useState("")
-
-   useEffect(()=>{
-    const fetchSubjectsAndChapters=async (cname)=>{
+  useEffect(() => {
+    const fetchSubjectsAndChapters = async () => {
       try {
-        console.log(cname)
-        const response = await axios.get('http://localhost:8000/taketest/getSubjects', {
-            withCredentials: true,
-            params:{
-              cname:cname
-            }
-  
-          });
-            
-            const data=response.data
-            console.log("sub is"+data[0].chaps)
-            setSubjects(data);
-           
+        const response = await axios.get("http://localhost:8000/taketest/getSubjects", {
+          withCredentials: true,
+          params: { cname },
+        });
+        setSubjects(response.data);
+      } catch (error) {
+        console.error("Error fetching subjects:", error);
+        setError(error.message);
+      }
+    };
 
-       
-    } catch (error) {
-        console.error('Error fetching subjects:', error);
-        setError(error.message)
-    }
-    }
-    fetchSubjectsAndChapters(cname);
-   },[])
- 
-  
+    fetchSubjectsAndChapters();
+  }, [cname]);
 
-
-  
-  
   return (
+    <>
+    <Navbar/>
     <div className="scourse-page">
       <h1 className="scourse-title">{cname}</h1>
-     
-     
-      {subjects && <div className="stopic-list">
-        {subjects.map((topic, index) => (
-          <>
-          <Topic key={index} index={index} name={topic.name} course={cname} />
-          
-            { topic.chaps.map((chapter)=>{
-              return <Link to={`${topic.name}/${chapter}`}>{chapter}</Link>
-            }) }
-          
-          </>
 
-        ))}
-      </div>
-        }    
-        {error && <p>{error}</p>} 
+      {subjects.length > 0 && (
+        <div className="stopic-list">
+          {subjects.map((topic, index) => (
+            <Topic
+              key={index}
+              index={index}
+              name={topic.name}
+              course={cname}
+              chapters={topic.chaps}
+            />
+          ))}
+        </div>
+      )}
+
+      {error && <p>{error}</p>}
     </div>
+    </>
   );
 };
 
