b11cefdc864f0b3305f8c3450375b547d566ba6d
Fahad Saleem <mfahadsal216@gmail.com>
2024-12-01 17:43:28 +0500

    message

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import './login.css'
 function LoginComponent() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -46,12 +46,36 @@ function LoginComponent() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-            <button type="submit">Login</button>
-             {error &&<p>{error}</p>}
-        </form>
+        <div className='login-page'>
+            <div className='login-text1'>
+                <p>
+                Your journey
+                <br />
+                Towards learning
+                <br />
+                Starts here!
+                </p>  
+            </div>
+
+            <div className='login-form-area'>
+                <div className="login-text2">
+                    <h1>Hi there!</h1>
+                    <p>Login to continue</p>
+                </div>
+                <div className='login-area'>
+                    <form className='login-area' onSubmit={handleSubmit}>
+                        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                        <a href="" className='forgot-password'>Forgot Password</a>
+                        <button type="submit">Login</button>
+                        {error &&<p>{error}</p>}
+                        <p>New to PrepMaster?</p>
+                        <a href=' 'className='signup-link'>Sign up now!!!</a>
+                    </form>
+                
+                </div>
+            </div>
+        </div>
     );
 }
 
